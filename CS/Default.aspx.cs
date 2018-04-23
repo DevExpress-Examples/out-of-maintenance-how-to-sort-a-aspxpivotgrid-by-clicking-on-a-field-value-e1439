75d19c947a19cbebd446280156776c9760d4f8e8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using DevExpress.Web.ASPxPivotGrid;
+using System.Text;
+using DevExpress.XtraPivotGrid;
+using DevExpress.XtraPivotGrid.Data;
+using System.Collections.Generic;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ASPxPivotGrid1.FieldValueTemplate = new FieldValueTemplate(ASPxPivotGrid1);
+    }
+    protected void ASPxPivotGrid1_CustomCallback(object sender, PivotGridCustomCallbackEventArgs e) {
+        string[] args = e.Parameters.Split('|');
+        if(args[0] == "SC")
+            HandleSortByColumnClick((ASPxPivotGrid)sender, args);
+    }
+
+    void HandleSortByColumnClick(ASPxPivotGrid pivotGrid, string[] args) {
+        int fieldIndex = int.Parse(args[1]),
+            visibleIndex = int.Parse(args[3]),
+            dataIndex = int.Parse(args[4]);
+        bool isColumn = bool.Parse(args[2]);
+        PivotArea area = GetArea(isColumn),
+            crossArea = GetCrossArea(isColumn);
+
+        PivotGridField dataField;
+        List<PivotGridField> fields;
+        List<object> values;
+        GetFieldsAndValues(pivotGrid, fieldIndex, visibleIndex, dataIndex, area, 
+            out dataField, out fields, out values);
+        
+        SetSortByColumn(pivotGrid, crossArea, dataField, fields, values);
+    }
+
+    void SetSortByColumn(ASPxPivotGrid pivotGrid, PivotArea crossArea, PivotGridField dataField, List<PivotGridField> fields, List<object> values) {
+        pivotGrid.BeginUpdate();
+        List<PivotGridField> crossFields = pivotGrid.GetFieldsByArea(crossArea);
+        for(int i = 0; i < crossFields.Count; i++) {
+            crossFields[i].SortBySummaryInfo.Field = dataField;
+            crossFields[i].SortBySummaryInfo.Conditions.Clear();
+            for(int j = 0; j < values.Count; j++) {
+                crossFields[i].SortBySummaryInfo.Conditions.Add(
+                    new PivotGridFieldSortCondition(fields[j], values[j]));
+            }
+        }
+        pivotGrid.EndUpdate();
+    }
+
+    void GetFieldsAndValues(ASPxPivotGrid pivotGrid, int fieldIndex, int visibleIndex, int dataIndex, PivotArea area, out PivotGridField dataField, out List<PivotGridField> fields, out List<object> values) {
+        dataField = pivotGrid.GetFieldByArea(PivotArea.DataArea, dataIndex);
+        fields = pivotGrid.GetFieldsByArea(area);
+        values = new List<object>(fields.Count);
+        for(int i = 0; i < fields.Count; i++) {
+            object value = pivotGrid.GetFieldValueByIndex(fields[i], visibleIndex);
+            values.Add(value);
+            if(fields[i].Index == fieldIndex) break;
+        }
+    }
+
+    PivotArea GetCrossArea(bool isColumn) {
+        return isColumn ? PivotArea.RowArea : PivotArea.ColumnArea;
+    }
+
+    PivotArea GetArea(bool isColumn) {
+        return isColumn ? PivotArea.ColumnArea : PivotArea.RowArea;
+    }
+}
+
+public class FieldValueTemplate : ITemplate {
+    public FieldValueTemplate(ASPxPivotGrid pivotGrid) {
+        this.pivotGrid = pivotGrid;
+    }
+
+    ASPxPivotGrid pivotGrid;
+    protected ASPxPivotGrid PivotGrid { get { return pivotGrid; } }
+
+    #region ITemplate Members
+
+    public void InstantiateIn(Control container) {
+        PivotGridFieldValueTemplateContainer c = (PivotGridFieldValueTemplateContainer)container;
+        HyperLink link = new HyperLink();
+        link.Text = (string)c.Text;
+        link.NavigateUrl = "#";
+        link.Attributes["onclick"] = GetOnClickHandler(c);
+        c.Controls.Add(link);
+        bool isSortedByColumn = GetIsSortedByColumn(c);
+        if(isSortedByColumn) {
+            c.Controls.Add(new LiteralControl("&nbsp;*"));
+        }
+    }
+
+    bool GetIsSortedByColumn(PivotGridFieldValueTemplateContainer c) {
+        List<PivotGridFieldPair> sortedFields = PivotGrid.Data.VisualItems.GetSortedBySummaryFields(c.ValueItem.IsColumn, c.ValueItem.Index);
+        bool isSortedByColumn = sortedFields != null && sortedFields.Count > 0;
+        return isSortedByColumn;
+    }
+
+    string GetOnClickHandler(PivotGridFieldValueTemplateContainer c) {
+        StringBuilder res = new StringBuilder();
+        res.Append(pivotGrid.ClientInstanceName).Append(".PerformCallback('SC|");
+        res.Append(GetFieldIndex(c)).Append("|")
+            .Append(c.ValueItem.IsColumn).Append("|")
+            .Append(c.ValueItem.VisibleIndex).Append("|")
+            .Append(c.ValueItem.DataIndex);
+        res.Append("');");
+        return res.ToString();
+    }
+
+    int GetFieldIndex(PivotGridFieldValueTemplateContainer c) {
+        return c.ValueItem.Field != null ? c.ValueItem.Field.Index : -1;
+    }
+
+    #endregion
+}
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,126 @@
+﻿Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxPivotGrid
+Imports System.Text
+Imports DevExpress.XtraPivotGrid
+Imports DevExpress.XtraPivotGrid.Data
+Imports System.Collections.Generic
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        ASPxPivotGrid1.FieldValueTemplate = New FieldValueTemplate(ASPxPivotGrid1)
+    End Sub
+    Protected Sub ASPxPivotGrid1_CustomCallback(ByVal sender As Object, ByVal e As PivotGridCustomCallbackEventArgs)
+        Dim args() As String = e.Parameters.Split("|"c)
+        If args(0) = "SC" Then
+            HandleSortByColumnClick(DirectCast(sender, ASPxPivotGrid), args)
+        End If
+    End Sub
+
+    Private Sub HandleSortByColumnClick(ByVal pivotGrid As ASPxPivotGrid, ByVal args() As String)
+        Dim fieldIndex As Integer = Integer.Parse(args(1)), visibleIndex As Integer = Integer.Parse(args(3)), dataIndex As Integer = Integer.Parse(args(4))
+        Dim isColumn As Boolean = Boolean.Parse(args(2))
+        Dim area As PivotArea = GetArea(isColumn), crossArea As PivotArea = GetCrossArea(isColumn)
+
+        Dim dataField As PivotGridField = Nothing
+        Dim fields As List(Of PivotGridField) = Nothing
+        Dim values As List(Of Object) = Nothing
+        GetFieldsAndValues(pivotGrid, fieldIndex, visibleIndex, dataIndex, area, dataField, fields, values)
+
+        SetSortByColumn(pivotGrid, crossArea, dataField, fields, values)
+    End Sub
+
+    Private Sub SetSortByColumn(ByVal pivotGrid As ASPxPivotGrid, ByVal crossArea As PivotArea, ByVal dataField As PivotGridField, ByVal fields As List(Of PivotGridField), ByVal values As List(Of Object))
+        pivotGrid.BeginUpdate()
+        Dim crossFields As List(Of PivotGridField) = pivotGrid.GetFieldsByArea(crossArea)
+        For i As Integer = 0 To crossFields.Count - 1
+            crossFields(i).SortBySummaryInfo.Field = dataField
+            crossFields(i).SortBySummaryInfo.Conditions.Clear()
+            For j As Integer = 0 To values.Count - 1
+                crossFields(i).SortBySummaryInfo.Conditions.Add(New PivotGridFieldSortCondition(fields(j), values(j)))
+            Next j
+        Next i
+        pivotGrid.EndUpdate()
+    End Sub
+
+    Private Sub GetFieldsAndValues(ByVal pivotGrid As ASPxPivotGrid, ByVal fieldIndex As Integer, ByVal visibleIndex As Integer, ByVal dataIndex As Integer, ByVal area As PivotArea, ByRef dataField As PivotGridField, ByRef fields As List(Of PivotGridField), ByRef values As List(Of Object))
+        dataField = pivotGrid.GetFieldByArea(PivotArea.DataArea, dataIndex)
+        fields = pivotGrid.GetFieldsByArea(area)
+        values = New List(Of Object)(fields.Count)
+        For i As Integer = 0 To fields.Count - 1
+            Dim value As Object = pivotGrid.GetFieldValueByIndex(fields(i), visibleIndex)
+            values.Add(value)
+            If fields(i).Index = fieldIndex Then
+                Exit For
+            End If
+        Next i
+    End Sub
+
+    Private Function GetCrossArea(ByVal isColumn As Boolean) As PivotArea
+        Return If(isColumn, PivotArea.RowArea, PivotArea.ColumnArea)
+    End Function
+
+    Private Function GetArea(ByVal isColumn As Boolean) As PivotArea
+        Return If(isColumn, PivotArea.ColumnArea, PivotArea.RowArea)
+    End Function
+End Class
+
+Public Class FieldValueTemplate
+    Implements ITemplate
+
+    Public Sub New(ByVal pivotGrid As ASPxPivotGrid)
+        Me.pivotGrid_Renamed = pivotGrid
+    End Sub
+
+
+    Private pivotGrid_Renamed As ASPxPivotGrid
+    Protected ReadOnly Property PivotGrid() As ASPxPivotGrid
+        Get
+            Return pivotGrid_Renamed
+        End Get
+    End Property
+
+    #Region "ITemplate Members"
+
+    Public Sub InstantiateIn(ByVal container As Control) Implements ITemplate.InstantiateIn
+        Dim c As PivotGridFieldValueTemplateContainer = CType(container, PivotGridFieldValueTemplateContainer)
+        Dim link As New HyperLink()
+        link.Text = CStr(c.Text)
+        link.NavigateUrl = "#"
+        link.Attributes("onclick") = GetOnClickHandler(c)
+        c.Controls.Add(link)
+        Dim isSortedByColumn As Boolean = GetIsSortedByColumn(c)
+        If isSortedByColumn Then
+            c.Controls.Add(New LiteralControl("&nbsp;*"))
+        End If
+    End Sub
+
+    Private Function GetIsSortedByColumn(ByVal c As PivotGridFieldValueTemplateContainer) As Boolean
+        Dim sortedFields As List(Of PivotGridFieldPair) = PivotGrid.Data.VisualItems.GetSortedBySummaryFields(c.ValueItem.IsColumn, c.ValueItem.Index)
+        Dim isSortedByColumn As Boolean = sortedFields IsNot Nothing AndAlso sortedFields.Count > 0
+        Return isSortedByColumn
+    End Function
+
+    Private Function GetOnClickHandler(ByVal c As PivotGridFieldValueTemplateContainer) As String
+        Dim res As New StringBuilder()
+        res.Append(pivotGrid_Renamed.ClientInstanceName).Append(".PerformCallback('SC|")
+        res.Append(GetFieldIndex(c)).Append("|").Append(c.ValueItem.IsColumn).Append("|").Append(c.ValueItem.VisibleIndex).Append("|").Append(c.ValueItem.DataIndex)
+        res.Append("');")
+        Return res.ToString()
+    End Function
+
+    Private Function GetFieldIndex(ByVal c As PivotGridFieldValueTemplateContainer) As Integer
+        Return If(c.ValueItem.Field IsNot Nothing, c.ValueItem.Field.Index, -1)
+    End Function
+
+    #End Region
+End Class
